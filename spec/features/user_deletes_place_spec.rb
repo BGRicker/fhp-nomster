4822c8775d8e8a22831740f43d4b42f26cca009d
Ben Ricker <BGRicker@gmail.com>
2016-04-30 21:19:57 -0400

    fix user deletion spec

--- spec/features/user_deletes_place_spec.rb
+++ spec/features/user_deletes_place_spec.rb
@@ -10,21 +10,6 @@ feature "User deletes a place" do
     expect(page).to have_content "185 Cool Street, Cooltown, CA"
     expect(page).to have_content "the coolest place"
 
-    click_on "Destroy"
-
-    expect(page).to_not have_content "Cooltown"
-    expect(page).to_not have_content "185 Cool Street, Cooltown, CA"
-    expect(page).to_not have_content "the coolest place"
-
-    # click_on "Delete"
-    # expect(page).to have_content "Edit Cooltown"
-    # update_place "Coolertown", "186 Cooler Street, Coolesttown, CA", "an even cooler place"
-
-    # expect(page).to_not have_content "Cooltown"
-    # expect(page).to_not have_content "185 Cool Street, Cooltown, CA"
-    # expect(page).to_not have_content "the coolest place"
-    # expect(page).to have_content "Coolertown"
-    # expect(page).to have_content "186 Cooler Street, Coolesttown, CA"
-    # expect(page).to have_content "an even cooler place"
+    expect { click_on 'Destroy' }.to change(Place, :count).by(-1)
   end
 end
