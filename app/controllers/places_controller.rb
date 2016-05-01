59c75b5576bc5ba089ac61663feeab852f1fcf59
Ben Ricker <BGRicker@gmail.com>
2016-04-30 23:02:29 -0400

    edit/update only by right user

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 5)
@@ -20,10 +20,16 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Must be place owner to edit', status: :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Must be place owner to edit', status: :forbidden
+    end
     @place.update_attributes(place_params)
     redirect_to place_path(@place)
   end
--- spec/features/places_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-feature "Pages index shows as root page" do
-  scenario "successfully" do
-    visit "/"
-    expect(page).to have_content('Welcome')
-  end
-end
